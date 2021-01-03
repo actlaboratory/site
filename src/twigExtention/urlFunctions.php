5bf01ea17ee3f035034793686912f485c5d15d8f
guredora <contact@guredora.com>
2021-01-03 15:13:20 +0900

    twig内でget_current_urlが呼ばれた時の処理を改善

--- src/twigExtention/urlFunctions.php
+++ src/twigExtention/urlFunctions.php
@@ -3,7 +3,13 @@ namespace src\twigExtention;
 
 class urlFunctions{
 	public static function get_current_url(){
-		return empty($_SERVER["HTTPS"]) ? "http://" : "https://".$_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+		$url = "http://";
+		if(!empty($_SERVER["HTTPS"])){
+			$url = "https://";
+		}
+		$url .= $_SERVER["HTTP_HOST"];
+		$url .= $_SERVER["REQUEST_URI"];
+		return $url;
 	}
 }
 ?>
