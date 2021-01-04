2624208b853d3039c452d6c8cf0709e2fff5b292
guredora <contact@guredora.com>
2021-01-05 02:04:02 +0900

    お知らせ配信時のURLに相対パスが指定されたときに絶対URLに直してtweetされるよう改善

--- app/Util/TwitterUtil.php
+++ app/Util/TwitterUtil.php
@@ -3,9 +3,13 @@ namespace Util;
 use Abraham\TwitterOAuth\TwitterOAuth;
 
 class TwitterUtil{
-	public static function tweet(string $text){
+	public static function tweet(string $text, string $url = ""){
+		$abs_url = $url;
+		if($url != ""){
+			$abs_url = UrlUtil::toAbsUrl($url);
+		}
 		$connection = new TwitterOAuth(getenv("TWITTER_API_KEY"), getenv("TWITTER_API_SECRET"), getenv("TWITTER_ACCESS_TOKEN"), getenv("TWITTER_ACCESS_TOKEN_SECRET"));
-		$result = $connection->post("statuses/update", ["status" => $text]);
+		$result = $connection->post("statuses/update", ["status" => $text."\n".$abs_url]);
 		return $result;
 	}
 }
