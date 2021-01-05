06dc15b4e616128f77e894a228e2f7403ab8f448
Kochi <c011610813@edu.teu.ac.jp>
2021-01-06 01:31:36 +0900

    指定した年のお知らせを抽出する関数を追加

--- app/Model/Dao/Informations.php
+++ app/Model/Dao/Informations.php
@@ -5,9 +5,18 @@ namespace Model\Dao;
 use Doctrine\DBAL\DBALException;
 use PDO;
 
+class Informations extends Dao{
 
-class Informations extends Dao
-{
-
-
+	//Žw’è‚µ‚½”N‚Ì‚à‚Ì‚ðŽæ“¾
+	public function getByYear(int $year){
+		$queryBuilder = parent::getQueryBuilder()
+			->select('*')
+			->from($this->_table_name)
+			->andWhere(strval('"'.intval($year)) . "-01-01\" <= `date`")
+			->andWhere(strval('"'.intval($year)) . "-12-31\" >= `date`")
+			->orderBy("date", "DESC");
+		//var_dump($queryBuilder->getsql());
+		//exit();
+		return $queryBuilder->execute()->FetchALL();
+	}
 }
